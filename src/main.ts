7040108c2809a1a788835237e8df4c2178cab492
Dami-js <sewejed@gmail.com>
2021-05-05 18:29:18 +0100

    added api documentation

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,27 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService);
+
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
 
-  const configService = app.get(ConfigService);
+  const config = new DocumentBuilder()
+    .setTitle('Plearn Api Documentation')
+    .setDescription('Plearn API description')
+    .setVersion('1.0')
+    .addTag('student ')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const port = configService.get('PORT');
   await app.listen(port);
 }
